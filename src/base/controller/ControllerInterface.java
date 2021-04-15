f1a333d09b54eafbc31a0efc6fbdea9d5707ab18
Minh Bui <55333649+minhbibui@users.noreply.github.com>
2021-04-14 20:32:22 -0400

    Update the interface

--- src/base/controller/ControllerInterface.java
+++ src/base/controller/ControllerInterface.java
@@ -2,8 +2,8 @@ package base.controller;
 
 public interface ControllerInterface
 {
-	public void addNewEdit(int groupIndex);
-	public void addNewGroup();
+	public void addNewEdit(String text,int editIndex, boolean isAddition );
+	public void addNewGroup(int groupIndex, int editIndex);
 	
 	public void undoEdit(int groupIndex, int editIndex);
 	public void undoGroupEdits(int groupIndex);
