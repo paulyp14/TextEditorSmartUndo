fc12e0ed0f412905997c092474df6a33be4a7c85
Andy N <50818989+andy-mtl@users.noreply.github.com>
2021-04-15 20:29:37 -0400

    added getters for some of the attributes/

--- src/base/model/GroupContainer.java
+++ src/base/model/GroupContainer.java
@@ -20,13 +20,22 @@ public class GroupContainer {
 		this.editContainer = container;
 	}
 
+	/**
+	 * Singleton access point
+	 * @return GroupContainer, the existing (and only) GroupContainer
+	 */
 	public static GroupContainer getContainer() {
+		// singleton access point
 		if (null == containerSingleton) {
 			containerSingleton = new GroupContainer(EditContainer.getContainer());
 		}
 		return containerSingleton;
 	}
-	
+
+	/**
+	 * Create a new user managed group of edits
+	 * @param groupName String the name of the group (MUST BE UNIQUE)
+	 */
 	public void create(String groupName) {
 		//creating a new EditGroup
 		if (this.groups.containsKey(groupName)) {
@@ -36,7 +45,12 @@ public class GroupContainer {
 		}
 		this.groups.put(groupName, new EditGroup(groupName));
 	}
-	
+
+	/**
+	 * Remove a user managed group of edits, without deleting the edits
+	 * (put any edits in the group back in the defaultGroup)
+	 * @param groupName String
+	 */
 	public void removeGroup(String groupName) {
 		/*
 			USER WANTS TO REMOVE THE GROUP, BUT NOT THE EDITS IN THE GROUP
@@ -53,6 +67,10 @@ public class GroupContainer {
 		this.defaultGroup.add(group.getEdits());
 	}
 
+	/**
+	 * Remove a user managed group of edits, and delete any edits that exist in the group
+	 * @param groupName String
+	 */
 	public void removeAndDeleteGroup(String groupName) {
 		/*
 			USER WANTS TO REMOVE THE GROUP, AND DELETE THE EDITS IN THE GROUP
@@ -69,7 +87,11 @@ public class GroupContainer {
 			this.editContainer.delete(e.getId());
 		}
 	}
-	
+
+	/**
+	 * Undo all edits in a group
+	 * @param groupName String
+	 */
 	public void undoGroup(String groupName) {
 		/*
 			USER WANTS TO UNDO ALL EDITS IN A SPECIFIC GROUP
@@ -87,7 +109,12 @@ public class GroupContainer {
 		}
 		this.editContainer.undoByIds(ids);
 	}
-	
+
+	/**
+	 * Add edits to a group
+	 * @param groupName String the group to add edits to
+	 * @param editIds list of EditIds to add to the group
+	 */
 	public void add(String groupName, ArrayList<Integer> editIds) {
 		// adding a specified list of Edits to the specified EditGroup
 		if (!this.groups.containsKey(groupName)) {
@@ -108,10 +135,46 @@ public class GroupContainer {
 		this.groups.get(groupName).add(editsToAdd);
 	}
 
+	/**
+	 * Add a single edit to a group
+	 * @param groupName String
+	 * @param editId int
+	 */
 	public void add(String groupName, int editId) {
 		this.add(groupName, new ArrayList<>(Collections.singletonList(editId)));
 	}
 
+	/**
+	 * Undo specific edits in a specific group
+	 * @param groupName String
+	 * @param editIds the list of edits to undo
+	 */
+	public void undoEditsInGroup(String groupName, ArrayList<Integer> editIds) {
+		if (!this.groups.containsKey(groupName)) {
+			System.out.println("ERROR: this group name does not exist");
+			// TODO this should be an exception
+			return;
+		}
+
+		this.editContainer.undoByIds(editIds);
+		this.update();
+	}
+
+	/**
+	 * Undo the edits that are in the defualt group
+	 * @param editIds
+	 */
+	public void undoEditsInDefaultGroup(ArrayList<Integer> editIds) {
+		this.editContainer.undoByIds(editIds);
+		this.update();
+	}
+
+	/**
+	 * Makes sure specific group is up to date
+	 * @param groupName
+	 * @param editGroup
+	 * @param existingIds
+	 */
 	private void syncEditGroup(String groupName, EditGroup editGroup, ArrayList<Integer> existingIds) {
 		// remove any edits that are no longer valid
 		ArrayList<Integer> editsToRemove = new ArrayList<>();
@@ -123,6 +186,10 @@ public class GroupContainer {
 		editGroup.removeById(editsToRemove);
 	}
 
+	/**
+	 * makes sure the default group is up to date
+	 * @param edits
+	 */
 	private void updateDefaultGroup(ArrayList<EditView> edits) {
 		ArrayList<Integer> idsInDefault = new ArrayList<>();
 		ArrayList<EditView> missing = new ArrayList<>();
@@ -135,6 +202,9 @@ public class GroupContainer {
 		this.defaultGroup.add(missing);
 	}
 
+	/**
+	 * Updates all groups if the editContainer was updated so that they accurately reflect the state
+	 */
 	public void update() {
 		if (this.editContainer.isUpdated()) {
 			// if an edit was deleted/undone/removed
@@ -156,6 +226,10 @@ public class GroupContainer {
 		}
 	}
 
+	/**
+	 *
+	 * @return string representation of the GroupContainer
+	 */
 	public String stringRepr() {
 		int maxLen = 60;
 		ArrayList<String> components = new ArrayList<>();
@@ -182,7 +256,41 @@ public class GroupContainer {
 		return String.join("\n", finalRepr);
 	}
 
+	/**
+	 * Get the representative EditViews for a specific group
+	 * @param groupName
+	 * @return
+	 */
+	public ArrayList<EditView> viewEditsInGroup(String groupName) {
+		if (!this.groups.containsKey(groupName)) {
+			System.out.println("ERROR: this group name does not exist");
+			// TODO this should be an exception
+			return null;
+		}
+		return this.groups.get(groupName).getEdits();
+	}
+
+	/**
+	 * Get the representative EditViews for the default group
+	 * @return
+	 */
 	public ArrayList<EditView> viewEditsInDefaultGroup() {
 		return this.defaultGroup.getEdits();
+	}
+	
+	/**
+	 * Getter for the groups attribute
+	 * @return groups attribute
+	 */
+	public HashMap<String, EditGroup> getGroups() {
+		return this.groups;
+	}
+	
+	/**
+	 * Getter for the editContainer attribute
+	 * @return editContainer attribute
+	 */
+	public EditContainer getEditContainer() {
+		return this.editContainer;
 	}
 }
