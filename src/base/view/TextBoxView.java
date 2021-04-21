18d658d7da7b27216583080518834ab5df8c3c65
Andy N <50818989+andy-mtl@users.noreply.github.com>
2021-04-21 00:12:09 -0400

    done but needs more comments

--- src/base/view/TextBoxView.java
+++ src/base/view/TextBoxView.java
@@ -8,7 +8,7 @@ import base.controller.TextBoxController;
  * View class that manages the user interface of 
  * the JTextArea.
  * 
- * @author Thanh Tung Nguyen
+ * @author Thanh Tung Nguyen, Andy Nguyen
  */
 public class TextBoxView extends JTextArea {
 
@@ -34,8 +34,58 @@ public class TextBoxView extends JTextArea {
     	controller.add(str, position, isAddition);
     }
     
+    public void readEdits(String prev, String temp) {
+    	// case where there was an insert in the document (previous tbox is shorter than current tbox)
+    	if (prev.length() < temp.length())
+    	{
+    		int i = 0; // i is used to detect the position of the insertion
+    		while (i < prev.length()) 
+    		{
+    			if (prev.charAt(i) != (temp.charAt(i)))
+    				break;
+    			i++;
+    		}
+    		if (i == prev.length()) // if the insertion at the end of the document
+    			controller.add(temp.substring(i, temp.length()), -1, true);
+    		else // if the insertion was not at the end
+    		{
+    			try {
+    			controller.add(temp.substring(i, temp.length()-prev.length()+i), i, true);
+    			} catch (Exception e) {
+    				i--;
+    				controller.add(temp.substring(i, temp.length()-prev.length()+i), i, true);
+    			}
+    		}
+    	}
+    	// case where there was a removal in the document (previous tbox is longer than current tbox)
+    	else if (prev.length() > temp.length())
+    	{
+    		int i = 0; // i is used to detect the position of the deletion
+    		while (i < temp.length()) 
+    		{
+    			if (prev.charAt(i) != (temp.charAt(i)))
+    				break;
+    			i++;
+    		}
+    		
+    		try {
+    			controller.add(prev.substring(i, prev.length()-temp.length()+i), i, false);
+    		} catch (Exception e) {
+    			i--;
+    			controller.add(prev.substring(i, prev.length()-prev.length()+i), i, false);
+    		}
+    	}
+    }
+    
     public void undo() {
     	controller.undo();
+    }
+    
+    /**
+     * Displays the content of both the editcontainer and groupcontainer on the console.
+     */
+    public void displayOnConsole() {
+    	controller.StringRepr();
     }
 
 
